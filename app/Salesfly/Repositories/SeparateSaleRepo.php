3a0891ccbe3584faf5cc75adcc472ce6e64f6f58
Javixo <jalvarez@plazaverde.pe>
2016-03-21 09:54:41 -0500

    buscar por codigo.prod en ventas y sep/ped

--- app/Salesfly/Repositories/SeparateSaleRepo.php
+++ app/Salesfly/Repositories/SeparateSaleRepo.php
@@ -32,6 +32,10 @@ class SeparateSaleRepo extends BaseRepo{
         //var_dump($q); die();
 
         $separate = SeparateSale::leftjoin('salePayments','salePayments.separateSale_id','=','separateSales.id')
+            ->join('detSeparateSales','detSeparateSales.separateSale_id','=','separateSales.id')
+            ->leftjoin('detPres','detPres.id','=','detSeparateSales.detPre_id')
+            ->leftjoin('variants','variants.id','=','detPres.variant_id')
+            ->leftjoin('products','products.id','=','variants.product_id')
             ->leftjoin('customers','separateSales.customer_id','=','customers.id')
             ->select('separateSales.*','salePayments.estado as estadoPago')
             ->with('customer','employee')
@@ -40,9 +44,11 @@ class SeparateSaleRepo extends BaseRepo{
                 $query->orWhere('separateSales.fechaPedido', 'like', $this->q . '%')
                     ->orWhere('customers.nombres', 'like', $this->q . '%')
                     ->orWhere('customers.apellidos', 'like', $this->q . '%')
-                    ->orWhere('customers.empresa', 'like', $this->q . '%');
+                    ->orWhere('customers.empresa', 'like', $this->q . '%')
+                    ->orWhere('products.codigo','like',$this->q . '%');
                     })
             ->whereIn('separateSales.tipo',$x)
+            ->groupBy('separateSales.id')
             ->orderBy('separateSales.id','DESC')
             ->paginate(15);
         return $separate;
